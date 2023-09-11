03192d58109e6b4fdfd6af70bdc58bf8544427a0
Ata Sahin <atasahin05@gmail.com>
2023-09-11 20:17:53 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { useState } from "react";
-import Landing from "./components/Landing";
-import index from "./index.css";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState("");
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&id=524901&appid=928b916fd33a31ae13bc9f5c43fee8af`;
+  const [location, setLocation] = useState("Istanbul");
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const apiKey = "928b916fd33a31ae13bc9f5c43fee8af";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+        setError(null);
+      })
+      .catch((error) => {
+        setError("Location not found");
+        setData({});
+      });
+  }, [location]);
 
   const searchLocation = (event) => {
-    if (event.key === "Enter")
-      axios.get(url).then((response) => {
-        setData(response.data);
-        console.log(response.data);
-      });
+    if (event.key === "Enter") {
+      setLocation(event.target.value);
+    }
   };
 
   return (
@@ -23,37 +35,43 @@ function App() {
           value={location}
           onChange={(event) => setLocation(event.target.value)}
           placeholder="Enter Location"
-          onKeyPress={searchLocation}
           type="text"
+          color="white"
         ></input>
+        <button onClick={() => setLocation(location)}>Search</button>
       </div>
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>Dallas</p>
-          </div>
-          <div className="temp">
-            <h1 className="bold">20 C</h1>
-          </div>
-          <div className="description">
-            <p>Clouds</p>
-          </div>
-        </div>
-        <div className="bottom">
-          <div className="feels">
-            <p>24 C</p>
-            <p>Feels Like</p>
-            <div className="humidity">
-              <p>20%</p>
-              <p>Humidity</p>
+      {error && <p>{error}</p>}
+      {data && data.name && (
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>
+                {data.name}, {data.sys.country}
+              </p>
             </div>
-            <div className="wind">
-              <p>20 km/h</p>
-              <p>Wind Speed</p>
+            <div className="temp">
+              <h1 className="bold">{Math.round(data.main.temp - 273.15)}°C</h1>
+            </div>
+            <div className="description">
+              <p>{data.weather[0].description}</p>
             </div>
           </div>
+          <div className="bottom">
+            <div className="feels">
+              <p>{Math.round(data.main.feels_like - 273.15)}°C</p>
+              <p>Feels Like</p>
+              <div className="humidity">
+                <p>{data.main.humidity}%</p>
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                <p>{data.wind.speed} m/s</p>
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
